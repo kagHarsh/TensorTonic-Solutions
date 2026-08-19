1cb14859b3b684af6c0ff3883f9f7007ba557116
Harsh Kag <124779369+kagHarsh@users.noreply.github.com>
2026-08-19 14:04:22 +0530

    Solve positional-encoding

---
+++ positional-encoding/positional-encoding.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def positional_encoding(seq_len, d_model, base=10000.0):
+    """
+    Return PE of shape (seq_len, d_model) using sin/cos formulation.
+    Odd d_model -> last column is sin.
+    """
+    # Write code here
+    
+    T, d = int(seq_len), int(d_model)
+    pos = np.arange(T, dtype=float)[:, None]
+    print("pos --> ", pos)
+    i = np.arange((d + 1) // 2, dtype=float)[None, :]
+    print("i --> ", i)
+    div = np.power(base, (2 * i) / d)
+    angles = pos / div
+    print("angles --> ", angles)
+    pe = np.zeros((T, d), dtype=float)
+    print("pe --> ", pe)
+    pe[:, 0::2] = np.sin(angles[:, :(d + 1) // 2])[:, :len(pe[0, 0::2])]
+    pe[:, 1::2] = np.cos(angles[:, :d // 2])[:, :len(pe[0, 1::2])]
+    return pe
+            
